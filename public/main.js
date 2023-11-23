c4ddd12d7b3c5f4b377df30fc659c1e26fe63bb3
DanielLazaro1555 <danielsam_m@hotmail.com>
2023-11-22 21:42:49 -0500

    Nuevo cambio

--- public/main.js
+++ public/main.js
@@ -1,71 +1,69 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Llamamos a la función loadJSON con la URL del archivo JSON en línea
-  loadJSON(
-    "https://raw.githubusercontent.com/DanielLazaro1555/Musica1/main/public/bd.json"
-  );
-});
-
-
-
-var currentSongIndex = 0; // Índice de la canción actual
-
-// Función para cargar el JSON y mostrar la primera canción
-function loadJSON(jsonPath) {
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        try {
-          var jsonData = JSON.parse(xhr.responseText);
-
-          // Verificamos si jsonData es un array o un objeto
-          if (Array.isArray(jsonData)) {
-            // Mostramos solo la primera canción al principio
-            displayCurrentSong(jsonData[currentSongIndex]);
-            // Agregamos un evento al botón "Siguiente"
-            var nextButton = document.getElementById("nextButton");
-            nextButton.addEventListener("click", function () {
-              // Incrementamos el índice de la canción actual
-              currentSongIndex++;
-              // Verificamos si el índice está fuera del rango
-              if (currentSongIndex >= jsonData.length) {
-                currentSongIndex = 0; // Volvemos al principio si llegamos al final
-              }
-              // Llamamos a la función para mostrar la nueva canción
+    // Llamamos a la función loadJSON con la URL del archivo JSON en línea
+    loadJSON(
+      "https://raw.githubusercontent.com/DanielLazaro1555/Musica1/main/public/bd.json"
+    );
+  });
+  
+  var currentSongIndex = 0; // Índice de la canción actual
+  
+  // Función para cargar el JSON y mostrar la primera canción
+  function loadJSON(jsonPath) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          try {
+            var jsonData = JSON.parse(xhr.responseText);
+  
+            // Verificamos si jsonData es un array o un objeto
+            if (Array.isArray(jsonData)) {
+              // Mostramos solo la primera canción al principio
               displayCurrentSong(jsonData[currentSongIndex]);
-            });
-          } else if (typeof jsonData === "object" && jsonData !== null) {
-            // Si es un objeto, creamos un array con ese objeto
-            displayCurrentSong(jsonData);
-          } else {
-            console.error(
-              "El archivo JSON no contiene un array de canciones ni un objeto válido."
-            );
+              // Agregamos un evento al botón "Siguiente"
+              var nextButton = document.getElementById("nextButton");
+              nextButton.addEventListener("click", function () {
+                // Incrementamos el índice de la canción actual
+                currentSongIndex++;
+                // Verificamos si el índice está fuera del rango
+                if (currentSongIndex >= jsonData.length) {
+                  currentSongIndex = 0; // Volvemos al principio si llegamos al final
+                }
+                // Llamamos a la función para mostrar la nueva canción
+                displayCurrentSong(jsonData[currentSongIndex]);
+              });
+            } else if (typeof jsonData === "object" && jsonData !== null) {
+              // Si es un objeto, creamos un array con ese objeto
+              displayCurrentSong(jsonData);
+            } else {
+              console.error(
+                "El archivo JSON no contiene un array de canciones ni un objeto válido."
+              );
+            }
+          } catch (error) {
+            console.error("Error al analizar el archivo JSON:", error);
           }
-        } catch (error) {
-          console.error("Error al analizar el archivo JSON:", error);
+        } else {
+          console.error(
+            "Error al cargar el archivo JSON. Código de estado:",
+            xhr.status
+          );
         }
-      } else {
-        console.error(
-          "Error al cargar el archivo JSON. Código de estado:",
-          xhr.status
-        );
       }
-    }
-  };
-  xhr.open("GET", jsonPath, true);
-  xhr.send();
-}
-
-// Función para mostrar la canción actual
-function displayCurrentSong(currentSong) {
-  var songsContainer = document.getElementById("songsContainer");
-  songsContainer.innerHTML = ""; // Limpiamos el contenedor
-
-  // Creamos la tarjeta de la canción
-  var songCard = document.createElement("div");
-  songCard.className = "card mb-3";
-  songCard.innerHTML = `
+    };
+    xhr.open("GET", jsonPath, true);
+    xhr.send();
+  }
+  
+  // Función para mostrar la canción actual
+  function displayCurrentSong(currentSong) {
+    var songsContainer = document.getElementById("songsContainer");
+    songsContainer.innerHTML = ""; // Limpiamos el contenedor
+  
+    // Creamos la tarjeta de la canción
+    var songCard = document.createElement("div");
+    songCard.className = "card mb-3";
+    songCard.innerHTML = `
           <div class="row g-0">
               <div class="col-md-4">
                   <img src="${getFullUrl(
@@ -92,27 +90,24 @@ function displayCurrentSong(currentSong) {
                       <p class="card-text"><strong>Derechos de autor:</strong><br> <span>${
                         currentSong.derechos_de_autor || "Desconocido"
                       }</span></p>
-                      <div class="d-flex justify-content-center mt-3">
-      <button id="prevButton" class="btn btn-secondary me-3">Anterior</button>
-      <audio controls class="w-100" style="width: 100%; background-color: #f8f9fa; border: 1px solid #dee2e6; border-radius: 4px;">
+                      
+                      <audio controls class="w-100" style="width: 100%; background-color: #f8f9fa; border: 1px solid #dee2e6; border-radius: 4px;">
                           <source src="${getFullUrl(
                             currentSong.archivo_musica
                           )}">
                           Tu navegador no soporta el elemento de audio.
                       </audio>
-      <button id="nextButton" class="btn btn-primary ms-3">Siguiente</button>
-                        </div>
-                      
                   </div>
               </div>
           </div>
       `;
-
-  songsContainer.appendChild(songCard);
-}
-
-// Función para obtener la URL completa basada en la ubicación actual del script
-function getFullUrl(relativeUrl) {
-  var base = window.location.href;
-  return new URL(relativeUrl, base).href;
-}
+  
+    songsContainer.appendChild(songCard);
+  }
+  
+  // Función para obtener la URL completa basada en la ubicación actual del script
+  function getFullUrl(relativeUrl) {
+    var base = window.location.href;
+    return new URL(relativeUrl, base).href;
+  }
+  
